55818d98b27fbe8071784a111b8896ca8f85f032
Massey <LMassey@teampurpose.com>
2022-07-05 16:32:20 -0400

    first commit weather app

---
+++ app.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="h-screen relative overflow-hidden">
+    <img />
+    <div class="absolute w-full h-full top-0 overlay" />
+    <div class="absolute w-full h-full top-0 p-48">
+      <div class="flex justify-between">
+        <div>
+          <h1 class="text-7xl text-white">Toronto</h1>
+          <p class="font-extralight text-2xl mt-2 text-white">
+            Sunday December 9, 2022
+          </p>
+          <img class="w-56 icon" />
+        </div>
+        <div>
+          <p class="text-9xl text-white font-extralight">58°</p>
+        </div>
+      </div>
+      <div class="mt-20">
+        <input type="text" class="w-1/2 h-10" placeholder="Search a city"/>
+        <button class="bg-sky-400 w-20 h-10 text-white">Search</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.overlay {
+  background-color: rgba(0, 0, 0, 0.5);
+}
+</style>
