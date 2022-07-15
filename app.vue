38db06c63e9d3ae85944d47e49ef0487db8a9e85
Nova Massey <nova.massey12@gmail.com>
2022-07-15 14:49:42 -0400

    weather-app complete

--- app.vue
+++ app.vue
@@ -1,25 +1,121 @@
+<script setup lang='ts'>
+
+const cookie = useCookie("city");
+const config= useRuntimeConfig();
+
+if (!cookie.value) cookie.value = "New York";
+
+const search = ref(cookie.value);
+const input = ref("");
+const background = ref("");
+
+
+
+// const {data:city, error}= useFetch(
+//   ()=>`https://api.openweathermap.org/data/2.5/weather?q=${search.value}&appid=e4a2009dd8f902daa35cc35a8ff96936`)
+
+const { data: city, error } = useAsyncData(
+  "city",
+  async () => {
+    let response;
+    try {
+      response = await $fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${search.value}`,
+        {
+          params:{
+            units:'metric',
+            appid: config.WEATHER_APP_SECRET,
+          }
+        }
+      );
+
+      cookie.value=search.value;
+
+      const temp = response.main.temp;
+
+      if (temp <= -10) {
+        background.value =
+          "https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+      } else if (temp > -10 && temp <= 0) {
+        background.value =
+          "https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+      } else if (temp > 0 && temp <= 10) {
+        background.value =
+          "https://images.unsplash.com/photo-1560258018-c7db7645254e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4032&q=80";
+      } else {
+        background.value =
+          "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3546&q=80";
+      }
+    } catch (error) {}
+    return response;
+  },{
+    watch:[search],
+  }
+);
+
+let today = new Date()
+
+today.toLocaleDateString("en-US", {
+  weekday:"long",
+  year: "numeric",
+  month: "long",
+  day:"numeric"
+})
+
+  const handleClick = () => {
+  const formattedSearch = input.value.trim().split(" ").join("+");
+  search.value = formattedSearch;
+  input.value = "";
+  console.log(search);
+  // refresh();
+};
+
+const goBack = () => {
+   search.value=cookie.value;
+}
+
+</script>
+
+
 <template>
-  <div class="h-screen relative overflow-hidden">
-    <img />
+  <div v-if="city" class="h-screen relative overflow-hidden">
+    <img :src="background" />
     <div class="absolute w-full h-full top-0 overlay" />
     <div class="absolute w-full h-full top-0 p-48">
       <div class="flex justify-between">
         <div>
-          <h1 class="text-7xl text-white">Toronto</h1>
+          <h1 class="text-7xl text-white">{{ city.name }}</h1>
           <p class="font-extralight text-2xl mt-2 text-white">
-            Sunday December 9, 2022
+            {{today}}
           </p>
-          <img class="w-56 icon" />
+
+          <img
+            :src="`https://openweathermap.org/img/wn/${city.weather[0].icon}@4x.png`"
+            class="w-56 icon"
+          />
         </div>
         <div>
-          <p class="text-9xl text-white font-extralight">58°</p>
+          <p class="text-9xl text-white font-extralight">
+            {{ city.main.temp }}°
+          </p>
         </div>
       </div>
       <div class="mt-20">
-        <input type="text" class="w-1/2 h-10" placeholder="Search a city"/>
-        <button class="bg-sky-400 w-20 h-10 text-white">Search</button>
+        <input
+          type="text"
+          class="w-1/2 h-10"
+          placeholder="Search a city"
+          v-model="input"
+        />
+        <button class="bg-sky-400 w-20 h-10 text-white" @click="handleClick">
+          Search
+        </button>
       </div>
     </div>
+  </div>
+  <div v-else class="p-10">
+    <h1 class="text-7xl text-red">Could not find that city!</h1>
+    <button class="mt-5 bg-sky-400 px-10 w-50 text0white h-10" @click="goBack">Go back</button>
   </div>
 </template>
 
@@ -27,4 +123,10 @@
 .overlay {
   background-color: rgba(0, 0, 0, 0.5);
 }
+
+.icon {
+  margin-left: 2.75rem;
+  margin-top: 2.75rem;
+}
 </style>
+
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    publicRuntimeConfig:{
+        WEATHER_APP_SECRET: process.env.WEATHER_APP_SECRET
+    },
+    privateRuntimeConfig:{
+        HELLO:"world in the server, not in client"
+    },
 css:[
     "~/assets/css/tailwind.css"
 ],
